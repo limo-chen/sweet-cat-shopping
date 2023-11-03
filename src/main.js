96c9ea3c89b302db76a5c98ce9ccf3203d37ca76
Calista <2320035651@qq.com>
2023-11-03 22:18:09 +0800

    实现吸顶导航交互

--- src/main.js
+++ src/main.js
@@ -5,10 +5,6 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
-import { getCotegory } from "@/apis/testAPI";
-getCotegory().then((res) => {
-  console.log(res);
-});
 const app = createApp(App);
 
 app.use(createPinia());
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,3 +1,3 @@
 <template>
-  <div>home</div>
+  <div style="height: 2000px">我是home页面</div>
 </template>

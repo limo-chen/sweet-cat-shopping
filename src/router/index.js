2863a2452a57d625226f481652ab292270b02501
Calista <2320035651@qq.com>
2023-11-08 23:06:34 +0800

    详情页路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Layout from "@/views/Layout/index.vue";
 import Home from "@/views/Home/index.vue";
 import Category from "@/views/Category/index.vue";
 import SubCategory from "@/views/SubCategory/index.vue";
+import Detail from "@/views/Detail/index.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -31,6 +32,10 @@ const router = createRouter({
     {
       path: "/login",
       component: Login,
+    },
+    {
+      path: "/detail/:id",
+      component: Detail,
     },
   ],
   //路由滚动行为定制

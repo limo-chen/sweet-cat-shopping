1581c66e9f8f49ff252f7c78c809b03080421922
Calista <2320035651@qq.com>
2023-11-08 21:34:15 +0800

    列表无限加载的实现

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -32,6 +32,20 @@ const tabChange = () => {
   reqData.value.page = 1;
   getGoodList();
 };
+//加载更多
+const disabled = ref(false);
+const load = async () => {
+  console.log("加载更多数据了");
+  //获取下一页数据
+  reqData.value.page++;
+  const res = await getSubCategoryAPI(reqData.value);
+  //拼接数组
+  goodList.value = [...goodList.value, ...res.result.items];
+  //加载完毕 停止监听
+  if (res.result.items.length === 0) {
+    disabled.value = true;
+  }
+};
 </script>
 
 <template>
@@ -52,7 +66,11 @@ const tabChange = () => {
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div
+        class="body"
+        v-infinite-scroll="load"
+        :infinite-scroll-disabled="disabled"
+      >
         <!-- 商品列表-->
         <GoodsItem v-for="goods in goodList" :goods="goods" :key="goods.id" />
       </div>

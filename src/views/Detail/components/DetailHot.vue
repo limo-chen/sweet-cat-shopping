af54c69baec37d730d68014ef84338f71d205468
Calista <2320035651@qq.com>
2023-11-09 20:56:21 +0800

    适配不同的title和数据列表

--- src/views/Detail/components/DetailHot.vue
+++ src/views/Detail/components/DetailHot.vue
@@ -1,13 +1,27 @@
 <script setup>
 import { getHotGoodsAPI } from "@/apis/detail";
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
 import { useRoute } from "vue-router";
+//设计props参数，适配不同的title和数据,hotType是一个props参数，它的类型是Number
+const props = defineProps({
+  hotType: {
+    type: Number,
+  },
+});
+
+//适配title  1-24小时热榜  2-周热榜
+const TYPEMAP = {
+  1: "24小时热榜",
+  2: "周热榜",
+};
+const title = computed(() => TYPEMAP[props.hotType]);
+//封装接口 调用接口渲染模板
 const hotList = ref([]);
 const route = useRoute();
 const getHotList = async () => {
   const res = await getHotGoodsAPI({
     id: route.params.id,
-    type: 1,
+    type: props.hotType,
   });
   hotList.value = res.result;
 };
@@ -17,7 +31,7 @@ onMounted(() => getHotList());
 
 <template>
   <div class="goods-hot">
-    <h3>周日榜单</h3>
+    <h3>{{ title }}</h3>
     <!-- 商品区块 -->
     <RouterLink
       to="/"

98bc5e9a59f6ae7298b0bad0e5de155a5bf9fdb9
Calista <2320035651@qq.com>
2023-11-09 17:36:48 +0800

    基础组件封装

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -1,4 +1,5 @@
 <script setup>
+import DetailHot from "./components/DetailHot.vue";
 import { getDetail } from "@/apis/detail";
 import { ref, onMounted } from "vue";
 import { useRoute } from "vue-router";
@@ -126,7 +127,12 @@ onMounted(() => getGoods());
               </div>
             </div>
             <!-- 24热榜+专题推荐 -->
-            <div class="goods-aside"></div>
+            <div class="goods-aside">
+              <!-- 24小时 -->
+              <DetailHot />
+              <!-- 周 -->
+              <DetailHot />
+            </div>
           </div>
         </div>
       </div>

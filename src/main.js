84c82b4af9c7fb10ce0334ea8a720ee61cba814b
Calista <2320035651@qq.com>
2023-11-14 18:24:22 +0800

    全局注册

--- src/main.js
+++ src/main.js
@@ -6,11 +6,14 @@ import { lazyPlugin } from "@/directives";
 
 import App from "./App.vue";
 import router from "./router";
+
+//引入全局组件组件
+import { componentPlugin } from "@/components";
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-
+app.use(componentPlugin);
 app.mount("#app");
 app.use(lazyPlugin);
 Vue.config.devtools = true;
--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -1,10 +1,9 @@
 <script setup>
 import DetailHot from "./components/DetailHot.vue";
-import ImageView from "@/components/ImageView/index.vue";
 import { getDetail } from "@/apis/detail";
 import { ref, onMounted } from "vue";
 import { useRoute } from "vue-router";
-import XtxSku from "@/components/XtxSku/index.vue";
+
 const goods = ref({});
 const route = useRoute();
 const getGoods = async () => {
@@ -46,7 +45,7 @@ const skuChange = (sku) => {
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-              <ImageView :image-list="goods.mainPictures" />
+              <XtxImageView :image-list="goods.mainPictures" />
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>

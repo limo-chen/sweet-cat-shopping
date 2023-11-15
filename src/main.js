cace453e2c7844c64e9c2fe4d7d67b793fdcfe70
Calista <2320035651@qq.com>
2023-11-15 19:13:59 +0800

    pinia持久化

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import "@/styles/common.scss";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { lazyPlugin } from "@/directives";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
 import App from "./App.vue";
 import router from "./router";
@@ -10,8 +11,11 @@ import router from "./router";
 //引入全局组件组件
 import { componentPlugin } from "@/components";
 const app = createApp(App);
-
-app.use(createPinia());
+const pinia = createPinia();
+console.log(pinia);
+//注册持久化插件
+pinia.use(piniaPluginPersistedstate);
+app.use(pinia);
 app.use(router);
 app.use(componentPlugin);
 app.mount("#app");
--- src/stores/user.js
+++ src/stores/user.js
@@ -3,19 +3,25 @@ import { defineStore } from "pinia";
 import { loginAPI } from "@/apis/user";
 import { ref } from "vue";
 //接受两个参数，一个是模块名，一个是回调函数，返回值依然是一个方法useUserStore
-export const useUserStore = defineStore("user", () => {
-  //1，定义管理用户数据的state
-  const userInfo = ref([]);
-  //2, 定义获取接口数据的action函数
-  const getUserInfo = async ({ account, password }) => {
-    const res = await loginAPI({ account, password });
-    userInfo.value = res.result;
-  };
-  //3，以对象格式把state和action return出去
-  return {
-    userInfo,
-    getUserInfo,
-  };
-});
+export const useUserStore = defineStore(
+  "user",
+  () => {
+    //1，定义管理用户数据的state
+    const userInfo = ref([]);
+    //2, 定义获取接口数据的action函数
+    const getUserInfo = async ({ account, password }) => {
+      const res = await loginAPI({ account, password });
+      userInfo.value = res.result;
+    };
+    //3，以对象格式把state和action return出去
+    return {
+      userInfo,
+      getUserInfo,
+    };
+  },
+  {
+    persist: true,
+  }
+);
 
 // 接下来在组件中只需要触发getUserInfo，getUserInfo一触发，接口就调用了，接口调用了，数据就存在userInfo中了

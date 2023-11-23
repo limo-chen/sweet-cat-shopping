d697787350424d7c1df7e9c87adf7425839e4e26
Calista <2320035651@qq.com>
2023-11-23 10:56:13 +0800

    生成订单功能实现

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import Category from "@/views/Category/index.vue";
 import SubCategory from "@/views/SubCategory/index.vue";
 import Detail from "@/views/Detail/index.vue";
 import CartList from "@/views/CartList/index.vue";
+import Pay from "@/views/Pay/index.vue";
 import Checkout from "@/views/Checkout/index.vue";
 
 const router = createRouter({
@@ -36,6 +37,10 @@ const router = createRouter({
         {
           path: "checkout",
           component: Checkout,
+        },
+        {
+          path: "pay",
+          component: Pay,
         },
       ],
     },

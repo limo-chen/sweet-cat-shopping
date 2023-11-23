06563383848e68e782649c5eba23f4ac39978a67
Calista <2320035651@qq.com>
2023-11-23 22:02:48 +0800

    使用基础参数获取基础列表

--- src/router/index.js
+++ src/router/index.js
@@ -12,8 +12,8 @@ import Pay from "@/views/Pay/index.vue";
 import Checkout from "@/views/Checkout/index.vue";
 import PayBack from "@/views/Pay/PayBack.vue";
 import Member from "@/views/Member/index.vue";
-import UserInfo from "@/views/Member/Components/UserInfo.vue";
-import UserOrder from "@/views/Member/Components/UserOrder.vue";
+import UserInfo from "@/views/Member/components/UserInfo.vue";
+import UserOrder from "@/views/Member/components/UserOrder.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),

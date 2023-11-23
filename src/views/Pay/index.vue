3cf6ff3f7725ba298f2dc86790b1e6f8be1af6e1
Calista <2320035651@qq.com>
2023-11-23 13:04:58 +0800

    支付结果显示

--- src/views/Pay/index.vue
+++ src/views/Pay/index.vue
@@ -15,7 +15,7 @@ onMounted(() => getPayInfo());
 //携带订单Id以及回调地址跳转到支付地址（get）
 // 支付地址
 const baseURL = "http://pcapi-xiaotuxian-front-devtest.itheima.net/";
-const backURL = "http://127.0.0.1:5173/paycallback";
+const backURL = "http://localhost:5173/paycallback";
 const redirectUrl = encodeURIComponent(backURL);
 const payUrl = `${baseURL}pay/aliPay?orderId=${route.query.id}&redirect=${redirectUrl}`;
 </script>
